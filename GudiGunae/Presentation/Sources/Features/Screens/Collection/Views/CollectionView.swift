b0d1fc5e7a122a9bcedd8a34bcc6cab1114a9f05
김정완 <krwd1117@gmail.com>
2025-03-05 16:53:52 +0900

    'CollectionView'에 'NavigationTitle'을 추가

--- GudiGunae/Presentation/Sources/Features/Screens/Collection/Views/CollectionView.swift
+++ GudiGunae/Presentation/Sources/Features/Screens/Collection/Views/CollectionView.swift
@@ -20,22 +20,26 @@ struct CollectionView: View {
     ]
     
     var body: some View {
-        ScrollView(.vertical) {
-            LazyVGrid(columns: columns, spacing: 10) {
-                ForEach(viewModel.filteredRestaurants) { restaurant in
-                    CollectionCellView(viewModel: CollectionCellViewModel(restaurant: restaurant))
-                        .onTapGesture {
-                            viewModel.selectedImageURL = restaurant.imageURL
-                            showDetailImage = true
-                        }
+        NavigationStack {
+            ScrollView(.vertical) {
+                LazyVGrid(columns: columns, spacing: 10) {
+                    ForEach(viewModel.filteredRestaurants) { restaurant in
+                        CollectionCellView(viewModel: CollectionCellViewModel(restaurant: restaurant))
+                            .onTapGesture {
+                                viewModel.selectedImageURL = restaurant.imageURL
+                                showDetailImage = true
+                            }
+                    }
                 }
+                .padding()
             }
-            .padding()
+            .background(Color.gray.opacity(0.2))
+            
+            .navigationTitle("모아보기")
         }
         .onReceive(bottomTabBarViewModel.$restaurants) { newRestaurants in
             viewModel.restaurants = newRestaurants
         }
-        .background(Color.gray.opacity(0.2))
         .fullScreenCover(isPresented: $showDetailImage) {
             if let imageURL = viewModel.selectedImageURL {
                 let viewModel = DetailImageViewModel(imageURL: imageURL)
